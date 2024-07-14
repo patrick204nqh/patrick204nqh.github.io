a06cf696efaa6ad92f5e84c21e2400461cbf7eba
Huy Nguyen <patrick204nqh@gmail.com>
2024-07-14 17:32:59 +0700

    chore: Update version number in _config.yml and copyright files

--- docs/_includes/copyright.js
+++ docs/_includes/copyright.js
@@ -1,5 +1,5 @@
 /*!
- * Personal Blog and Portfolio 0.0.1 by Nguyen Quang Huy
+ * Personal Blog and Portfolio {{ site.version }} by Nguyen Quang Huy
  * Copyright 2024 Nguyen Quang Huy - patrick204nqh.github.io
  * Free for personal and commercial use under the MIT license
  * https://github.com/patrick204nqh/patrick204nqh.github.io/blob/main/LICENSE
